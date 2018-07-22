5dc02a9218743d52135d8fbd1438be87d5d33dfb
Nautman <doug.beng-2019@vrg.se>
2018-07-22 15:56:36 +0200

    Index.js file

---
+++ index.js
@@ -0,0 +1,10 @@
+// get express module and store it in express variable
+const express = require('express')
+// create app variable that is a function of express
+const app = express()
+
+// on website "/"" directory, i.e. "example.net/", send a result (res), "Hello World!"
+app.get('/', (req, res) => res.send('Hello World!'))
+
+// create server that listens to port 3000 and when server has started console.log that it has started
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
