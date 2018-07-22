8c872b5eea1578793e260857e3a83007df319278
Zigolox <simonsonden@gmail.com>
2018-07-22 16:10:04 +0200

    added sendfile

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const express = require('express')
 // create app variable that is a function of express
 const app = express()
 
+var path = require('path');
+
 // on website "/"" directory, i.e. "example.net/", send a result (res), "Hello World!"
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
+
 
 // create server that listens to port 3000 and when server has started console.log that it has started
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
-
-app.get('/simon', (req, res) => res.send('hej simon'))
 
 
