2a65a77cd9206471a862820be40fd196837d45d4
Nautman <doug.beng-2019@vrg.se>
2018-07-24 00:47:18 +0200

    Added client side

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Search from './Shares.js'
+// import { Alert } from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Search />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/Shares.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './shares.scss'
+
+class SearchInput extends Component {
+	render() {
+		return (
+			<input />
+			)
+	}
+}
+
+class Search extends Component {
+	render() {
+		return (
+			<div class="search">
+				<SearchInput />
+			</div>
+			)
+	}
+}
+
+export default Search
